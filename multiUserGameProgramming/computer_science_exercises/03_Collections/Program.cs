b5676350906b581e317a4d24a6e541acf33707c3
Kenneth Whitfield <77504666+S11635372@users.noreply.github.com>
2023-10-03 14:27:02 -0400

    v0.1a
    added three arrays.

--- multiUserGameProgramming/computer_science_exercises/03_Collections/Program.cs
+++ multiUserGameProgramming/computer_science_exercises/03_Collections/Program.cs
@@ -1,4 +1,4 @@
-﻿//02 Collections, Kenneth Whitfield, v0.0
+﻿//02 Collections, Kenneth Whitfield, v0.1a
 using System;
 
 namespace _02_Collections
@@ -7,7 +7,21 @@ namespace _02_Collections
     {
         static void Main(string[] args)
         {
-            
+            // collectons are variabler that can store multiple values in variable
+
+            /*
+            -- Number of elements in an array cannot be changed
+            -- items in an array are called elements 
+            --contents of elements in an array can be changed
+            -- arrayes are ordered, meaning each item has a fixed location
+            -- the position is known as the index
+            -- the first elelment of an array is index 0
+            */
+
+            //Declaring and definig an array
+            sting[] breakfastFoods = {"Bacon", "Waffles", "Pancakes", "Cereal", "Parfait"};
+            int[] testScores = {95, 100, 25, 15, 27, 35};
+            float[] GPA = {3.14f,2.25f, 1.74f, 1.99f, 0.99f, 4.25 }
         }
     }
 }
