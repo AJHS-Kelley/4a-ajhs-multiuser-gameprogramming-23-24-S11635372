bba96c7bd05ba51834610053b12c77a63037a465
Kenneth Whitfield <77504666+S11635372@users.noreply.github.com>
2023-10-24 14:09:04 -0400

    code 1
    Made a method

---
+++ multiUserGameProgramming/04a_methodParamoters/programTemplate - Copy.cs
@@ -0,0 +1,22 @@
+//Kenneth Whitfield, Template, v0.
+using System;
+
+namespace MethodsParameters
+{
+    class MethodsParameters
+    {
+        //Method -- named block of codem can be used over and over agian
+        // all methods have a signature theat defines their name, parametersm and output
+        //example signature
+        static void  MyMethod()
+        {
+            Console.WriteLine("Fortnite Battlepass");
+            
+        }
+
+        static void Main(string[] args)
+        {
+            MyMethod();
+        }
+    }
+}
