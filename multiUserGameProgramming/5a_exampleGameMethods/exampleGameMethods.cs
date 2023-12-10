4706fe17fbc3ae47db96e565a3aa80194581f321
Kenneth Whitfield <77504666+S11635372@users.noreply.github.com>
2023-12-09 22:47:49 -0500

    started healing code

--- multiUserGameProgramming/5a_exampleGameMethods/exampleGameMethods.cs
+++ multiUserGameProgramming/5a_exampleGameMethods/exampleGameMethods.cs
@@ -1,23 +1,31 @@
 //Kenneth Whitfield, Template, v0.
 using System;
 
-namespace BlackJack
+namespace BasicCombatSim
 {
     class Template
     
     {
-        static int Inventory()//What is your inventory
+        static string Inventory()//What is your inventory
         {
-            string[] inventory = {"Mini potion", "Normal potion", "Military Grade potion", "Sword",}
+            string[] inventory = {"mini potion", "normal potion", "military grade potion"}
             return inventory
-        }
-        static int Healing()//Decides how much you can heal at onece
-        {
-            pass
         }
         static int Combat()//Decide damage
         {
             pass
+        }
+        static int Healing(inventory, health)//Decides how much you can heal at onece
+        {
+            healAmount = Console.ReadLine("You have a " + inventory + " In your inventory which would you like to use. To go back to combat type back")
+            if (healAmount = "military grade potion"){
+                health + 10
+            } if else(healAmount = "normal potion"){
+                health + 5
+            } if else(healAmount = "mini potion" ){
+                health + 2
+            } if else(healAmount = "back")
+            
         }
         static void Ending()//Decide who wins
         {
