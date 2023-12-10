fb0b9726aaf30515eabcaa560aa40908b11a359d
Kenneth Whitfield <77504666+S11635372@users.noreply.github.com>
2023-12-09 21:34:09 -0500

    Create exampleGameMethods.cs

---
+++ multiUserGameProgramming/5a_exampleGameMethods/exampleGameMethods.cs
@@ -0,0 +1,31 @@
+//Kenneth Whitfield, Template, v0.
+using System;
+
+namespace Template
+{
+    class Template
+    
+    {
+        static bool MergeFruit()
+        {
+            
+            
+        }
+        static int DropFruit()
+        {
+            
+        }
+        static int Points()
+        {
+            
+        }
+        static void Death()
+        {
+            
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
