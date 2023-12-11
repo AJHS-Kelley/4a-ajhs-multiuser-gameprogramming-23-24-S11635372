f1102b844a669a06900cfa1612d06a31072dd332
Kenneth Whitfield <77504666+S11635372@users.noreply.github.com>
2023-12-10 23:35:12 -0500

    added combat code
    and fixed semi colems

--- multiUserGameProgramming/5a_exampleGameMethods/exampleGameMethods.cs
+++ multiUserGameProgramming/5a_exampleGameMethods/exampleGameMethods.cs
@@ -8,22 +8,39 @@ namespace BasicCombatSim
     {
         static string Inventory()//What is your inventory
         {
-            string[] inventory = {"mini potion", "normal potion", "military grade potion"}
-            return inventory
+            string[] inventory = {"mini potion", "normal potion", "military grade potion"};
+            return inventory;
         }
         static int Combat()//Decide damage
         {
-            pass
+            int gokuHealth = 50;
+            int health = 40;
+
+            Random playerRoll = new Random();
+            int playerRolls = playerRoll.Next(1, 7);
+
+            Random gokuRoll = new Random();
+            int gokuRolls = gokuRoll.Next(1, 7);
+
+            if (playerRolls > gokuRolls){
+                gokuHealth - playerRolls;
+            } else if (playerRolls <= gokuRolls){
+                health - gokuRolls;
+            } else{
+                Console.WriteLine("ERROR");
+            }
+            
         }
-        static int Healing(inventory, health)//Decides how much you can heal at onece
+        static int Healing(string inventory,int health)//Decides how much you can heal at onece
         {
             healAmount = Console.ReadLine("You have a " + inventory + " In your inventory which would you like to use. To go back to combat type back")
-            if (healAmount = "military grade potion"){
-                health + 10
+           
+            if(healAmount = "military grade potion"){
+                health + 10;
             } if else(healAmount = "normal potion"){
-                health + 5
+                health + 5;
             } if else(healAmount = "mini potion" ){
-                health + 2
+                health + 2;
             } if else(healAmount = "back")
             
         }
