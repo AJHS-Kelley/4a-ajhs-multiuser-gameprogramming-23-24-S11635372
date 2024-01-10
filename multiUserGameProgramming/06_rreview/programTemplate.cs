6732447cb776d7b6664e61157332e36d68c02962
Kenneth Whitfield <77504666+S11635372@users.noreply.github.com>
2024-01-10 14:05:05 -0500

    v0.1
    yeah i did things

---
+++ multiUserGameProgramming/06_rreview/programTemplate.cs
@@ -0,0 +1,35 @@
+//Kenneth Whitfield, Template, v0.
+using System;
+
+namespace ReviewProject
+{
+    class ReviewProject
+    {
+        static void Main(string[] args)
+        {
+            // decalaring and assigning variables
+            //interger -- -2,147,483,648 to 2,147,483,647
+            // Interger 4 bytes
+            int persona = 5;
+            //Long-- -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+            //Long 8 bytes
+            long myLong = 2
+            //float -- stores 6-7 decimal digits
+            // Float 4 bytes
+            float two = 1.999999F;
+            //Double stores up to  15 decimal digits
+            double myDouble = 8.2983423809
+            //Boolean -- true/false
+            // boolean = 1 bit
+            bool happy = false;
+            //string
+            //2-bytes per charecter
+            //must be in  " "
+            string personaFiveRoyal = "I litteraly look Joker Persona 5";
+            //charecter (char)
+            //Single charecter, in Single quotes ' '
+            //2-bytes
+            char myChar = 'M'
+        }
+    }
+}
